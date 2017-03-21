3451e7f30297a1b99831e910db91d0de35f0be8a
VargaDavid <varga.david.gamf@gmail.com>
2017-03-21 09:53:37 +0100

    Skeleton js for the project

---
+++ static/list_boards.js
@@ -0,0 +1,15 @@
+/**
+ * Created by codeorgo-vd on 2017.03.20..
+ */
+
+function listBoards() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.getItem("boards") == null) {
+            document.getElementById("boards").innerHTML = "You don't have any boards yet";
+        } else {
+            document.getElementById("boards").innerHTML = "You have some boards";
+        }
+    } else {
+        document.getElementById("boards").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
