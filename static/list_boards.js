4f5b092aba694dbb92fb2c18cb1b7686a84bce99
VargaDavid <varga.david.gamf@gmail.com>
2017-03-21 11:42:41 +0100

    Adding titles to local storing implemented

--- static/list_boards.js
+++ static/list_boards.js
@@ -4,12 +4,32 @@
 
 function listBoards() {
     if(typeof(Storage) !== "undefined") {
-        if (localStorage.getItem("boards") == null) {
+        if (localStorage.getItem("title") == null) {
             document.getElementById("boards").innerHTML = "You don't have any boards yet";
         } else {
-            document.getElementById("boards").innerHTML = "You have some boards";
+            printBoards();
         }
     } else {
         document.getElementById("boards").innerHTML = "Sorry, your browser does not support web storage...";
     }
-}
+    };
+
+$(document).ready(function(){
+    listBoards();
+});
+
+function printBoards() {
+    var title_array = localStorage.getItem("title");
+    for(var i = 0; i < title_array.length; i++) {
+        document.getElementById("boards").innerHTML = title_array[i];
+    }
+};
+
+function addNewBoard() {
+    var title = document.getElementById("title");
+    var title_list = JSON.parse(localStorage.getItem("title"));
+    var index = title_list.length;
+    title_list[index] = title.value;
+    localStorage.setItem("title", JSON.stringify(title_list));
+    var test = JSON.parse(localStorage.getItem("title"));
+};
