920fbbcf98e36d5b69993fe017580207a638871f
Gawnby <gombaspeteer@gmail.com>
2017-04-05 10:37:34 +0200

    fix changes from development

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from get_db import *
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates", static_url_path="/static",
--- get_db.py
+++ get_db.py
@@ -1,4 +1,20 @@
-from peewee import  *
+from peewee import *
 
-class BaseModel(model):
-    pass
+psql_db = PostgresqlDatabase(database_name, user=database_user)
+
+
+class BaseModel(Model):
+
+    class Meta:
+        database = db
+
+
+class Boards(BaseModel):
+
+    title = CharField()
+
+
+class Cards(BaseModel):
+
+    title = CharField()
+    board_title = ForeignKeyField(Boards, related_name='boards_list')
